7ea34d9d6fc7d067a75bc9c153612088cb287dd2
Stanislav Lisovskiy <estalis@elx6k7qd12-0e.(none)>
2016-09-12 14:38:42 +0300

    sdfds

--- wsgi.py
+++ wsgi.py
@@ -36,7 +36,7 @@ def application(environ, start_response):
         s += d["W"][0]
         Wout = eval(d["W"][0])
         step = eval(d["step"][0])
-#        p = Predictor(int(d["points_per_network"]), Wout, int(d["num_layers"][0]), step, int(d["max_iterations"]))
+        p = Predictor(int(d["points_per_network"]), Wout, int(d["num_layers"][0]), step, int(d["max_iterations"]))
         ctype = 'text/html'
         s = s.replace("\n"," <br> ")
         s = s.replace("\r"," <br> ")
