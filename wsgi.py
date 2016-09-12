f2852d7a984f8aac1e4174d2962517c1fa530617
Stanislav Lisovskiy <estalis@elx6k7qd12-0e.(none)>
2016-09-12 15:14:32 +0300

    dasda

--- wsgi.py
+++ wsgi.py
@@ -56,8 +56,8 @@ def application(environ, start_response):
         s += d["W"][0]
         Wout = eval(d["W"][0])
         step = eval(d["step"][0])
-#        n = predictorAllocator.allocate(int(d["points_per_network"][0]), Wout, int(d["num_layers"][0]), step, int(d["max_iterations"][0]))
-#        s+=" Predictor created "+ str(n)
+        n = predictorAllocator.allocate(int(d["points_per_network"][0]), Wout, int(d["num_layers"][0]), step, int(d["max_iterations"][0]))
+        s+=" Predictor created "+ str(n)
         ctype = 'text/html'
         s = s.replace("\n"," <br> ")
         s = s.replace("\r"," <br> ")
